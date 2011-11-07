be5410e7ed9a8cda7c599917bebc49ed6cc257eb
Tatu Saloranta <tsaloranta@gmail.com>
2011-11-06 22:35:59 -0800

    Add sources, fix build: should be usable module...

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-== Overview
+## Overview
 
 This Jackson extension handles reading and writing of data using [Smile](http://wiki.fasterxml.com/SmileFormatSpec) data format ("binary JSON").
 It extends standard Jackson streaming API (`JsonFactory`, `JsonParser`, `JsonGenerator`), and as such works seamlessly with all the higher level data abstractions (data binding, tree model, and pluggable extensions).
 
-== Status
+## Status
 
 Module is fully usable.
 
-== Usage
-
-### Maven dependency
+## Maven dependency
 
 To use this extension on Maven-based projects, use following dependency:
 
@@ -21,10 +19,9 @@ To use this extension on Maven-based projects, use following dependency:
 
 (or whatever version is most up-to-date at the moment)
 
-### Usage
+## Usage
 
 Basic usage is by using `SmileFactory` in places where you would usually use `JsonFactory`:
-
 
     SmileFactory f = new SmileFactory();
     // can configure instance with 'SmileParser.Feature' and 'SmileGenerator.Feature'
---
+++ src/test/java/com/fasterxml/jackson/dataformat/smile/TestSmileFeatures.java
@@ -0,0 +1,37 @@
+package com.fasterxml.jackson.dataformat.smile;
+
+import org.codehaus.jackson.map.*;
+
+import com.fasterxml.jackson.dataformat.smile.SmileFactory;
+
+public class TestSmileFeatures
+    extends SmileTestBase
+{
+    /*
+    /**********************************************************
+    /* Helper types
+    /**********************************************************
+     */
+
+    static class Bean {
+        public int value;
+    }
+    
+    /*
+    /**********************************************************
+    /* Unit tests
+    /**********************************************************
+     */
+    // Let's ensure indentation doesn't break anything (should be NOP)
+    public void testIndent() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper(new SmileFactory());
+        mapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+        Bean bean = new Bean();
+        bean.value = 42;
+        
+        byte[] smile = mapper.writeValueAsBytes(bean);
+        Bean result = mapper.readValue(smile, 0, smile.length, Bean.class);
+        assertEquals(42, result.value);
+    }
+}
