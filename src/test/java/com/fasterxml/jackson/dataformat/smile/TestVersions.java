0097db663f9a190254d24d847ebcfb4699ede5bf
Tatu Saloranta <tsaloranta@gmail.com>
2013-02-28 14:55:07 -0800

    Upgrade version to 2.2, replace version lookup mechanism

--- src/test/java/com/fasterxml/jackson/dataformat/smile/TestVersions.java
+++ src/test/java/com/fasterxml/jackson/dataformat/smile/TestVersions.java
@@ -2,7 +2,6 @@ package com.fasterxml.jackson.dataformat.smile;
 
 import java.io.*;
 
-import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.Versioned;
 
 /**
@@ -10,13 +9,6 @@ import com.fasterxml.jackson.core.Versioned;
  */
 public class TestVersions extends SmileTestBase
 {
-    // Not the perfect way to do this, but has to do, for now...
-    private final static int MAJOR_VERSION = 2;
-    private final static int MINOR_VERSION = 1;
-
-    private final static String GROUP_ID = "com.fasterxml.jackson.dataformat";
-    private final static String ARTIFACT_ID = "jackson-dataformat-smile";
-    
     public void testMapperVersions() throws IOException
     {
         SmileFactory f = new SmileFactory();
@@ -31,17 +23,10 @@ public class TestVersions extends SmileTestBase
     /* Helper methods
     /**********************************************************
      */
-    
-    private void assertVersion(Versioned vers)
+
+    private void assertVersion(Versioned v)
     {
-        final Version v = vers.version();
-        assertFalse("Should find version information (got "+v+")", v.isUknownVersion());
-        assertEquals(MAJOR_VERSION, v.getMajorVersion());
-        assertEquals(MINOR_VERSION, v.getMinorVersion());
-        // Check patch level initially, comment out for maint versions
-//        assertEquals(0, v.getPatchLevel());
-        assertEquals(GROUP_ID, v.getGroupId());
-        assertEquals(ARTIFACT_ID, v.getArtifactId());
+        assertEquals(PackageVersion.VERSION, v.version());
     }
 }
 
